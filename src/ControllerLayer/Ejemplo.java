ac4c9809685e1d2e54a8d3eb46c20594d80b6216
Frangeris <frangeris_29@hotmail.com>
2011-07-17 17:27:17 -0400

    modificacion del archivo

--- src/ControllerLayer/Ejemplo.java
+++ src/ControllerLayer/Ejemplo.java
@@ -1,13 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package ControllerLayer;
 
-/**
- *
- * @author Administrador
- */
 public class Ejemplo {
-    
 }
